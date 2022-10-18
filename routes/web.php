545f96bbbbfa692f3f68fef6efbabe9a7e454fd1
Mohamedelaribi <mohamed.elaribi.solicode@gmail.com>
2022-10-18 17:01:10 +0100

    push

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CrudController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::get('/index',function(){
+    return view('index');
+});
+
+
+use Illuminate\Http\Request;
+Route::any('/form', function(Request $resquest){
+    $name = $resquest->Input('name');
+    return $name;
+
+});
