50f4998072768eba3250086e95ad821e3a3aa573
Mohamedelaribi <mohamed.elaribi.solicode@gmail.com>
2022-12-22 16:27:23 +0100

    push

---
+++ prototype-basic/app/Http/Controllers/TodoManager.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Todo;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class TodoManager extends Controller
+{
+
+    public function index()
+    {
+        $getTodo = Todo::all();
+        return view('todo', compact('getTodo'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    }
+
+
+    public function store(Request $request)
+    {
+        if(Auth::check()){
+            $addTodo = new Todo();
+            $addTodo->name = $request->name;
+            $addTodo->save();
+            return redirect('/todo');
+        }
+        else{
+            return redirect('/login');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
